37d89ef65a55d39b829dabdda5f01ec814208c11
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-05-05 11:51:03 -0700

    plants array

---
+++ plants-data.js
@@ -0,0 +1,29 @@
+export const plants = [
+    {
+        image: './assets/pacific-trillium.png',
+        name: 'Pacific Trillium',
+        phylum: 'Tracheophyta',
+        class: 'Liliopsida',
+        order: 'Liliales',
+        family: 'Melanthiaceae',
+        genus: 'Trillium',
+    },
+    {
+        image: './assets/western-sword-fern.png',
+        name: 'Western Sword Fern',
+        phylum: 'Tracheophyta',
+        class: 'Polypodiopsida',
+        order: 'Polypodiales',
+        family: 'Dryopteridaceae',
+        genus: 'Polystichum',
+    },
+    {
+        image: './assets/salal.png',
+        nme: 'Salal',
+        phylum: 'Tracheophyta',
+        class: 'Magnoliopsida',
+        order: 'Ericales',
+        family: 'Ericaceae',
+        genus: 'Gaultheria',
+    },
+];
