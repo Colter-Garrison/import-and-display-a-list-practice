087c2b036ba6149a94c30abfb48e36c9b994487e
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-05-06 16:53:46 -0700

    mollusk test is a go

--- test/example.test.js
+++ test/example.test.js
@@ -1,6 +1,8 @@
 // IMPORT MODULES under test here:
 import { plants } from '../plants-data.js';
 import { renderPlants } from '../renderPlants.js';
+import { mollusks } from '../mollusks-data.js';
+import { renderMollusks } from '../renderMollusks.js';
 
 const test = QUnit.test;
 
@@ -12,6 +14,20 @@ test('test plants data', (expect) => {
     //Act 
     // Call the function you're testing and set the result to a const
     const actual = renderPlants(plants[0]);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual.outerHTML, expected);
+});
+
+test('test mollusks data', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = `<div class="mollusk"><h1>Pacific Banana Slug</h1><img src="./assets/banana-slug.png"><p>This mollusk is native to Oregon and can grow up to 25cm.</p><h3>Comonly found in: </h3><ul><li>Alaska</li><li>Oregon</li><li>California</li></ul></div>`;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = renderMollusks(mollusks[0]);
 
     //Expect
     // Make assertions about what is expected versus the actual result
